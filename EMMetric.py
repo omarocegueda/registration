Added double gradient option (the average of the fixed- and moving- images' gradients), this makes sense for multimodal images only in the EM formulation

EMMetric.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import scipy as sp
 import tensorFieldUtils as tf
@@ -12,19 +13,21 @@ class EMMetric(SimilarityMetric):
     def getDefaultParameters(self):
         return {'lambda':1.0, 'maxInnerIter':5, 'scale':1, 
                 'maxStepLength':0.25, 'sigmaDiff':3.0, 'stepType':0, 
-                'qLevels':256, 'symmetric':False}
+                'qLevels':256, 'symmetric':False,'useDoubleGradient':False}
 
     def __init__(self, parameters):
         super(EMMetric, self).__init__(parameters)
         self.setSymmetric(self.parameters['symmetric'])
         self.stepType=self.parameters['stepType']
         self.quantizationLevels=self.parameters['qLevels']
+        self.useDoubleGradient=self.parameters['useDoubleGradient']
         self.fixedImageMask=None
         self.movingImageMask=None
         self.fixedQMeansField=None
         self.movingQMeansField=None
         self.movingQLevels=None
         self.fixedQLevels=None
+        
 
     def initializeIteration(self):
         samplingMask=self.fixedImageMask*self.movingImageMask
@@ -54,8 +57,15 @@ class EMMetric(SimilarityMetric):
         for grad in sp.gradient(self.fixedImage):
             self.gradientFixed[...,i]=grad
             i+=1
-        if not self.symmetric:#Quantization of the moving image and its corresponding statistics are used only for the backward step
-            return
+        if self.useDoubleGradient:
+            i=0
+            for grad in sp.gradient(self.fixedQMeansField):
+                self.gradientMoving[...,i]+=grad
+                i+=1
+            i=0
+            for grad in sp.gradient(self.movingQMeansField):
+                self.gradientFixed[...,i]=grad
+                i+=1
         if self.dim==2:
             movingQ, self.movingQLevels, hist=tf.quantizePositiveImageCYTHON(self.movingImage, self.quantizationLevels)
             movingQ=np.array(movingQ, dtype=np.int32)

RegistrationOptimizer.py
@@ -223,10 +223,10 @@ class RegistrationOptimizer(object):
         return self.forwardModel.getBackward()
 
 def testRegistrationOptimizerMonomodal2D():
-    fname0='data/circle.png'
-    fname1='data/C.png'
-    nib_moving=plt.imread(fname0)
-    nib_fixed=plt.imread(fname1)
+    fnameMoving='data/circle.png'
+    fnameFixed='data/C.png'
+    nib_moving=plt.imread(fnameMoving)
+    nib_fixed=plt.imread(fnameFixed)
     moving=nib_moving[:,:,0].astype(np.float64)
     fixed=nib_fixed[:,:,1].astype(np.float64)
     moving=np.copy(moving, order='C')
@@ -271,20 +271,39 @@ def testRegistrationOptimizerMultimodal2D(lambdaParam, synthetic):
     displacementGTName='templateToIBSR01_GT.npy'
     fnameMoving='data/t2/IBSR_t2template_to_01.nii.gz'
     fnameFixed='data/t1/IBSR_template_to_01.nii.gz'
-    nib_moving = nib.load(fnameMoving)
-    nib_fixed = nib.load(fnameFixed)
-    moving=nib_moving.get_data().squeeze().astype(np.float64)
-    fixed=nib_fixed.get_data().squeeze().astype(np.float64)
-    moving=np.copy(moving, order='C')
-    fixed=np.copy(fixed, order='C')
-    sl=moving.shape
-    sr=fixed.shape    
-    moving=moving[:,sl[1]//2,:].copy()
-    fixed=fixed[:,sr[1]//2,:].copy()
-    moving=(moving-moving.min())/(moving.max()-moving.min())
-    fixed=(fixed-fixed.min())/(fixed.max()-fixed.min())
-    maxIter=[i for i in [25,50,100,100]]
-    similarityMetric=EMMetric({'symmetric':True, 'lambda':250.0, 'stepType':SSDMetric.GAUSS_SEIDEL_STEP, 'qLevels':256, 'maxInnerIter':5})    
+#    fnameMoving='data/circle.png'
+#    fnameFixed='data/C.png'
+    nifti=True
+    if nifti:
+        nib_moving = nib.load(fnameMoving)
+        nib_fixed = nib.load(fnameFixed)
+        moving=nib_moving.get_data().squeeze().astype(np.float64)
+        fixed=nib_fixed.get_data().squeeze().astype(np.float64)
+        moving=np.copy(moving, order='C')
+        fixed=np.copy(fixed, order='C')
+        sl=moving.shape
+        sr=fixed.shape    
+        moving=moving[:,sl[1]//2,:].copy()
+        fixed=fixed[:,sr[1]//2,:].copy()
+        moving=(moving-moving.min())/(moving.max()-moving.min())
+        fixed=(fixed-fixed.min())/(fixed.max()-fixed.min())
+    else:
+        nib_moving=plt.imread(fnameMoving)
+        nib_fixed=plt.imread(fnameFixed)
+        moving=nib_moving[:,:,0].astype(np.float64)
+        fixed=nib_fixed[:,:,1].astype(np.float64)
+        moving=np.copy(moving, order='C')
+        fixed=np.copy(fixed, order='C')
+        moving=(moving-moving.min())/(moving.max() - moving.min())
+        fixed=(fixed-fixed.min())/(fixed.max() - fixed.min())
+    #maxIter=[i for i in [25,50,100,100]]
+    maxIter=[i for i in [10,13,25,25]]
+    similarityMetric=EMMetric({'symmetric':True, 
+                               'lambda':250.0, 
+                               'stepType':SSDMetric.GAUSS_SEIDEL_STEP, 
+                               'qLevels':256, 
+                               'maxInnerIter':5,
+                               'useDoubleGradient':True})    
     updateRule=UpdateRule.Composition()
     if(synthetic):
         print 'Generating synthetic field...'
@@ -354,8 +373,8 @@ def testRegistrationOptimizerMultimodal2D(lambdaParam, synthetic):
     print 'Mean displacement error: ', meanDisplacementError,'(',stdevDisplacementError,')'
 
 if __name__=='__main__':
-    #testRegistrationOptimizerMultimodal2D(150, True)
-    testRegistrationOptimizerMonomodal2D()
+    testRegistrationOptimizerMultimodal2D(150, True)
+    #testRegistrationOptimizerMonomodal2D()
     
 #    import nibabel as nib
 #    result=nib.load('data/circleToC.nii.gz')

registrationDiffeomorphic.py
@@ -726,7 +726,7 @@ def testInversion(lambdaParam):
         np.save(circleToCDisplacementName, displacement)
         np.save(circleToCDisplacementInverseName, inverse)
     print 'vector field exponential'
-    expd, invexpd=tf.vector_field_exponential(displacement)
+    expd, invexpd=tf.vector_field_exponential(displacement, True)
     print 'vector field inversion'
     directInverse=tf.invert_vector_field(displacement, 1.0, 10000, 1e-7)
     print 'vector field inversion'
@@ -747,7 +747,7 @@ def testInversion(lambdaParam):
     tf.write_double_buffer(np.array(displacement).reshape(-1), '../inverse/experiments/displacement_clean.bin')
 
 def testInversion_invertible():
-    displacement_clean=tf.create_invertible_displacement_field(256, 256, 0.05, 8)
+    displacement_clean=tf.create_invertible_displacement_field(256, 256, 0.5, 8)
     detJacobian=rcommon.computeJacobianField(displacement_clean)
     plt.figure()
     plt.imshow(detJacobian)
@@ -756,14 +756,30 @@ def testInversion_invertible():
     CS=plt.contour(X0,X1,detJacobian,levels=[0.0], colors='b')
     plt.clabel(CS, inline=1, fontsize=10)
     plt.title('det(J(displacement))')
-    #displacement=displacement_clean+np.random.normal(0.0, 0.0, displacement_clean.shape)
-    displacement=np.array(displacement_clean)
+    displacement=displacement_clean+np.random.normal(0.0, 1.1, displacement_clean.shape)
+    #displacement=np.array(displacement_clean)
     #inverse=rcommon.invert_vector_field_fixed_point(displacement, 100, 1e-7)
     #inverse=np.array(tf.invert_vector_field(displacement, 0.1, 100, 1e-7))
+    lambdaParam=5.0
+    #########Jacobi##########
+    inverse=np.array(tf.invert_vector_field(displacement, lambdaParam, 100, 1e-6))
+    residual, stats=tf.compose_vector_fields(displacement_clean, inverse)
+    residual=np.array(residual)
+    print 'Jacobi. Max:',stats[0], '. Mean:',stats[1],'Std:',stats[2]
+    [d,invd,res, detJ]=rcommon.plotDiffeomorphism(displacement, inverse, residual, 'Jacobi', 7)
+    #########Fixed point######
     inverse=np.array(tf.invert_vector_field_fixed_point(displacement, 100, 1e-6))
     residual, stats=tf.compose_vector_fields(displacement_clean, inverse)
     residual=np.array(residual)
-    [d,invd,res, detJ]=rcommon.plotDiffeomorphism(displacement, inverse, residual, 'invertible', 7)
+    print 'Fixed point. Max:',stats[0], '. Mean:',stats[1],'Std:',stats[2]
+    [d,invd,res, detJ]=rcommon.plotDiffeomorphism(displacement, inverse, residual, 'Fixed point', 7)
+    #########TV-L2###########
+    inverse=np.array(tf.invert_vector_field_tv_l2(displacement, lambdaParam, 3000, 1e-6))
+    residual, stats=tf.compose_vector_fields(displacement_clean, inverse)
+    residual=np.array(residual)
+    print 'TV-L2. Max:',stats[0], '. Mean:',stats[1],'Std:',stats[2]
+    [d,invd,res, detJ]=rcommon.plotDiffeomorphism(displacement, inverse, residual, 'TV-L2', 7)
+    
 
 #python registrationDiffeomorphic.py "/opt/registration/data/t1/IBSR18/IBSR_01/IBSR_01_ana_strip.nii.gz" "/opt/registration/data/t1/IBSR18/IBSR_02/IBSR_02_ana_strip.nii.gz" "IBSR_01_ana_strip_IBSR_02_ana_stripAffine.txt" "warp" 100.0
 if __name__=='__main__':