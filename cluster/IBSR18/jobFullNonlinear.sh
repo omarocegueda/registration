0a5219d485babf167b9479b4881da2f17894e179
omarocegueda <jomaroceguedag@gmail.com>
2014-01-22 11:24:36 -0500

    Apparently it does not recognize pmem... changed to mem

--- cluster/IBSR18/jobFullNonlinear.sh
+++ cluster/IBSR18/jobFullNonlinear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ####################################################
 # Author: Omar Ocegueda (omar@cimat.mx)
-#PBS -l pmem=4gb
+#PBS -l mem=4gb
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=03:00:00
 #PBS -N FullNonlinear
