033adc6d259a1e872f6441a2a9c5177a94227653
Omar Ocegueda <jomaroceguedag@gmail.com>
2014-02-07 09:49:58 -0600

    Use only one processor and the same number of iterations as SyNEM

--- cluster/IBSR18/jobFullANTS.sh
+++ cluster/IBSR18/jobFullANTS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ####################################################
 # Author: Omar Ocegueda (omar@cimat.mx)
-#PBS -l mem=4gb
-#PBS -l nodes=1:ppn=4
+#PBS -l mem=2gb
+#PBS -l nodes=1:ppn=1
 #PBS -l walltime=03:00:00
 #PBS -N FullANTS
 #PBS -M omar@cimat.com
@@ -10,7 +10,7 @@ export PATH="/opt/python/anaconda/bin:$PATH"
 export PYTHONPATH="/opt/python/anaconda/lib/python2.7/site-packages:/export/opt/python/anaconda/lib/python2.7/site-packages:$PYTHONPATH:$HOME/code/registration"
 export PATH=$HOME/opt:$PATH
 export PYTHONPATH=$HOME/opt/lib/python2.7/site-packages:$PYTHONPATH
-export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=4
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
 ###################################
 date
 reference=$(ls reference)
@@ -36,7 +36,7 @@ deformationField=${targetbase}_${referencebase}Warp.nii.gz
 if [ -r $deformationField ]; then
     echo "Deformation found. Registration skipped."
 else
-    exe="ANTS 3 -m  MI[reference/$reference,target/$target,1,3] -t SyN[0.5] -a ${affine} -r Gauss[2,0] -o ${targetbase}_${referencebase} -i 1x1x1 --continue-affine false"
+    exe="ANTS 3 -m  MI[reference/$reference,target/$target,1,3] -t SyN[0.25] -a ${affine} -r Gauss[2,0] -o ${targetbase}_${referencebase} -i 100x100x25 --continue-affine false"
     echo " $exe "
     $exe
 fi
