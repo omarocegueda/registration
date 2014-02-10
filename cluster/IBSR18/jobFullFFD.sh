1931eadcc1d66b5f76e20d2f53619bf1102625ce
Omar Ocegueda <jomaroceguedag@gmail.com>
2014-02-09 20:48:09 -0600

    Increase the verbose level and setting the number of threads to 1 in FFD shell script

--- cluster/IBSR18/jobFullFFD.sh
+++ cluster/IBSR18/jobFullFFD.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+nthreads=1
 ####################################################
 # Author: Omar Ocegueda (omar@cimat.mx)
 #PBS -l mem=2gb
@@ -17,19 +18,16 @@ targetbase="${targetbase%.*}"
 referencebase="${reference%.*}"
 referencebase="${referencebase%.*}"
 #Diffeomorphic registration
-exe0="cmtk registration --auto-multi-levels 4 --nmi --dofs 6,9 -o affine_directory reference/${reference} target/${target}"
-echo " $exe0 "
+exe0="cmtk registration --echo --threads ${nthreads} --verbose-level 2 --auto-multi-levels 4 --nmi --dofs 6,9 -o affine_directory reference/${reference} target/${target}"
 $exe0
 deformationField=${targetbase}_${referencebase}.xform
-exe1="cmtk warp --force-outside-value 0 --grid-spacing 40 --coarsest 4 --relax-to-unfold --refine 3 --jacobian-weight 1e-4 --ncc -e 16 -a 0.125 -o ${deformationField} affine_directory"
-echo " $exe1 "
+exe1="cmtk warp --echo --threads ${nthreads} --verbose-level 2 --force-outside-value 0 --grid-spacing 40 --coarsest 4 --relax-to-unfold --refine 3 --jacobian-weight 1e-4 --ncc -e 16 -a 0.125 -o ${deformationField} affine_directory"
 $exe1
 for towarp in $( ls warp ); do
     towarpbase="${towarp%.*}"
     towarpbase="${towarpbase%.*}"
     oname=${towarpbase}_${referencebase}.nii.gz
-    exe2="cmtk reformatx --nn -o $oname --floating warp/$towarp reference/${reference} ${deformationField}"
-    echo " $exe2 "
+    exe2="cmtk --echo --threads ${nthreads} --verbose-level 2 reformatx --nn -o $oname --floating warp/$towarp reference/${reference} ${deformationField}"
     $exe2
 done
 date
