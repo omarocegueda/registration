aa8c49bc2a13dd52826eb2599b54ee39a0f33707
omarocegueda <jomaroceguedag@gmail.com>
2014-03-10 22:26:08 -0400

    Fixed jaccard pairs naming

--- cluster/IBSR18/jobFullSyNEM.sh
+++ cluster/IBSR18/jobFullSyNEM.sh
@@ -31,5 +31,6 @@ else
     echo "Affine mapping found ($affine). Skipping affine registration."
 fi
 #Diffeomorphic registration
-python dipyreg.py target/$target reference/$reference $affine warp --metric=EM[0.25,25.0,256,20,demons]
+#python dipyreg.py target/$target reference/$reference $affine warp --metric=EM[0.25,25.0,256,20,v_cycle]
+python dipyreg.py target/$target reference/$reference $affine warp --metric=EM[0.25,1.0,256,20,demons]
 date
